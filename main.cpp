b607d249bc58ed3cd22b31fd5f5dbeab1e7c4018
tcs0051 <tcs0051@auburn.edu>
2022-11-14 18:20:30 -0500

    new

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
+
+void signUp (string 'user', string 'pswd'); // asks the user to sign up & adds user/pswd to excel sheet
 
 bool logIn (String 'user', String 'pswd'); // checks if user & pswd are entered correctly
 
@@ -16,44 +20,55 @@ int addAnother (int classes, int type);
 
 int main() {
 
-   int logIn(int user, int pswd) {
-        cout << "Enter your username:";
-        cin >> user;
-        cout << "Enter your password:";
-        cin >> pswd;
+    void signUP (string 'user', string 'pswd') {
+        cout << "Create a username and password\n"
+        << "Enter in your new username:";
+        cin >> 'user';
+        cout << "Enter in your new password:";
+        cin >> 'pswd';
+        \\ somewhere adds to text file   
+    }
 
-        if (user && pswd = correct) // checks excel sheet
+   bool logIn(string 'user', string 'pswd') {
+        cout << "Enter your username:";
+        cin >> 'user';
+        cout << "Enter your password:";
+        cin >> 'pswd';
+
+        if ('user' == 'user' && 'pswd' = 'pswd') // checks excel sheet
             cout << "Welcome to tiger scheduling!";
         else
-            cout << "Incorrect username or password";
+            cout << "Incorrect username or password. Enter again.\n"; // can it go through this more than once?
     }
 
     void addClass (int classes, int type) {
-       cout << "Enter class type (aero):" // from excel
-       int displayList () {
-       cout << "Enter class you want to add (aero 2000):"; }
-       bool seatsLeft (int seats) { // checks on seat availability
+       cout << "Enter class type (aero, biol, engr, finc, math):"; // from excel
+       cin >> type;
+       // reads file for list of classes under this type
+
+       int displayList (int class) { // displays list of this class type
+       cout << "Enter the class you want to add (aero 2000):";
+       cin >> class;
+    }
+
+       bool seatsLeft (int seats) { // checks on seat availability for file
            switch (seats)
                case 1:
                    if (seats > 0)
                        addClass to array
                        break;
            case 2:
-               if (seats < 0)
+               if (seats <= 0)
                    cout << "Added to waitlist";
                break;
-           case 3:
-               if (seats == 0)
-                   cout << "Can't add class. Seats are full";
-               break;
-
        }
+
        addAnother () {
-           while (char = y)
+           while (char = 'y')
                do
-                   cout << "Do you want to add another class? (y or n)"
+                   cout << "Do you want to add another class? (y or n)";
                    else 
-                       displaySchedule ()
+                       displaySchedule (int class, int type)
        }
    }
 
